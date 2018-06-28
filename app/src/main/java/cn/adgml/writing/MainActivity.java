93eda7cd58d3da10ea27e61a0b2f74e5d33c9046
zty0510 <2506752942@qq.com>
2018-06-28 22:54:24 +0800

    First commit
    6

---
+++ app/src/main/java/cn/adgml/writing/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.adgml.writing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}--cd-to-home
